f41fa0d3c98b80022069b19e263fdf0d44e53f05
Alex Curtis <37004191+kacurtis@users.noreply.github.com>
2024-07-18 21:18:55 -0700

    streamlining files

--- deploy-shinyapp-script.r
+++
@@ -1,6 +0,0 @@
-# deploy shiny app with rsconnect
-# first restart R session for clean slate
-devtools::install_github("kacurtis/ObsCovgTools")
-rsconnect::deployApp(appDir="inst/shinyapp/obscov", appName="obscov")
-
-# may need to add server info if more than one to choose from (account, server variables)
